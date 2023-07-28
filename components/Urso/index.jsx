03d2abcb8f44e9653cb70f832e4c66918ac23caf
marcoshmarcarini <marcoshmarcarini@hotmail.com>
2023-07-27 22:39:07 -0300

    Update index.jsx

--- components/Urso/index.jsx
+++ components/Urso/index.jsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 export default function Urso(){
     return(
         <div className={styles.Urso}>
-            <Image src={`/images/ursinhov2.png`} width={500} height={500} alt={'Ursinho'} />
+            <Image src={`/images/ursinhov2.png`} width={350} height={350} alt={'Ursinho'} />
         </div>
     )
 }
